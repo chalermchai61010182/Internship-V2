319873881c0504e8a78b5f46b6751be5fd9b2f86
chalermchai61010182 <61010182@kmitl.ac.th>
2021-08-17 03:38:05 +0700

    Create BGimage.js

---
+++ pages/src/BGimage.js
@@ -0,0 +1,12 @@
+// import {useEffect,useState}from 'react'
+// import Image from 'next/image'
+// import styledf from 'styled-components'
+
+// function BGImage() {
+//     const [width, setWidth]= useState<number>();
+//     const [height , setheight] = useState<number>();
+
+// return();
+// }
+
+// export default BG;
